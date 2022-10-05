235490d7ea932a89569eb644d8a8b2bab1446342
Pravin Rahangdale <pravinrahangdale41@gmail.com>
2022-10-06 00:09:36 +0530

    Add article and profile pages

---
+++ src/Pages/Article/Articletitle_.elm
@@ -0,0 +1,336 @@
+module Pages.Article.Articletitle_ exposing (Model, Msg, page)
+
+import Effect exposing (Effect)
+import Html exposing (..)
+import Html.Attributes as Attr
+import Layout exposing (Layout)
+import Page exposing (Page)
+import Route exposing (Route)
+import Shared
+import View exposing (View)
+
+
+layout : Layout
+layout =
+    Layout.NavBar
+
+
+page : Shared.Model -> Route { articletitle : String } -> Page Model Msg
+page shared route =
+    Page.new
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Effect Msg )
+init () =
+    ( {}
+    , Effect.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ExampleMsgReplaceMe
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        ExampleMsgReplaceMe ->
+            ( model
+            , Effect.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    { title = "Pages.Article.Articletitle_"
+    , body = [ viewBody ]
+    }
+
+
+viewBody : Html msg
+viewBody =
+    div
+        [ Attr.class "article-page"
+        ]
+        [ div
+            [ Attr.class "banner"
+            ]
+            [ div
+                [ Attr.class "container"
+                ]
+                [ h1 []
+                    [ text "How to build webapps that scale" ]
+                , div
+                    [ Attr.class "article-meta"
+                    ]
+                    [ a
+                        [ Attr.href ""
+                        ]
+                        [ img
+                            [ Attr.src "http://i.imgur.com/Qr71crq.jpg"
+                            ]
+                            []
+                        ]
+                    , div
+                        [ Attr.class "info"
+                        ]
+                        [ a
+                            [ Attr.href ""
+                            , Attr.class "author"
+                            ]
+                            [ text "Eric Simons" ]
+                        , span
+                            [ Attr.class "date"
+                            ]
+                            [ text "January 20th" ]
+                        ]
+                    , button
+                        [ Attr.class "btn btn-sm btn-outline-secondary"
+                        ]
+                        [ i
+                            [ Attr.class "ion-plus-round"
+                            ]
+                            []
+                        , text "Follow Eric Simons"
+                        , span
+                            [ Attr.class "counter"
+                            ]
+                            [ text "(10)" ]
+                        ]
+                    , button
+                        [ Attr.class "btn btn-sm btn-outline-primary"
+                        ]
+                        [ i
+                            [ Attr.class "ion-heart"
+                            ]
+                            []
+                        , text "Favorite Post"
+                        , span
+                            [ Attr.class "counter"
+                            ]
+                            [ text "(29)" ]
+                        ]
+                    ]
+                ]
+            ]
+        , div
+            [ Attr.class "container page"
+            ]
+            [ div
+                [ Attr.class "row article-content"
+                ]
+                [ div
+                    [ Attr.class "col-md-12"
+                    ]
+                    [ p []
+                        [ text "Web development technologies have evolved at an incredible clip over the past few years." ]
+                    , h2
+                        [ Attr.id "introducing-ionic"
+                        ]
+                        [ text "Introducing RealWorld." ]
+                    , p []
+                        [ text "It's a great solution for learning how other frameworks work." ]
+                    ]
+                ]
+            , hr []
+                []
+            , div
+                [ Attr.class "article-actions"
+                ]
+                [ div
+                    [ Attr.class "article-meta"
+                    ]
+                    [ a
+                        [ Attr.href "profile.html"
+                        ]
+                        [ img
+                            [ Attr.src "http://i.imgur.com/Qr71crq.jpg"
+                            ]
+                            []
+                        ]
+                    , div
+                        [ Attr.class "info"
+                        ]
+                        [ a
+                            [ Attr.href ""
+                            , Attr.class "author"
+                            ]
+                            [ text "Eric Simons" ]
+                        , span
+                            [ Attr.class "date"
+                            ]
+                            [ text "January 20th" ]
+                        ]
+                    , button
+                        [ Attr.class "btn btn-sm btn-outline-secondary"
+                        ]
+                        [ i
+                            [ Attr.class "ion-plus-round"
+                            ]
+                            []
+                        , text "Follow Eric Simons"
+                        ]
+                    , button
+                        [ Attr.class "btn btn-sm btn-outline-primary"
+                        ]
+                        [ i
+                            [ Attr.class "ion-heart"
+                            ]
+                            []
+                        , text "Favorite Post"
+                        , span
+                            [ Attr.class "counter"
+                            ]
+                            [ text "(29)" ]
+                        ]
+                    ]
+                ]
+            , div
+                [ Attr.class "row"
+                ]
+                [ div
+                    [ Attr.class "col-xs-12 col-md-8 offset-md-2"
+                    ]
+                    [ form
+                        [ Attr.class "card comment-form"
+                        ]
+                        [ div
+                            [ Attr.class "card-block"
+                            ]
+                            [ textarea
+                                [ Attr.class "form-control"
+                                , Attr.placeholder "Write a comment..."
+                                , Attr.rows 3
+                                ]
+                                []
+                            ]
+                        , div
+                            [ Attr.class "card-footer"
+                            ]
+                            [ img
+                                [ Attr.src "http://i.imgur.com/Qr71crq.jpg"
+                                , Attr.class "comment-author-img"
+                                ]
+                                []
+                            , button
+                                [ Attr.class "btn btn-sm btn-primary"
+                                ]
+                                [ text "Post Comment" ]
+                            ]
+                        ]
+                    , div
+                        [ Attr.class "card"
+                        ]
+                        [ div
+                            [ Attr.class "card-block"
+                            ]
+                            [ p
+                                [ Attr.class "card-text"
+                                ]
+                                [ text "With supporting text below as a natural lead-in to additional content." ]
+                            ]
+                        , div
+                            [ Attr.class "card-footer"
+                            ]
+                            [ a
+                                [ Attr.href ""
+                                , Attr.class "comment-author"
+                                ]
+                                [ img
+                                    [ Attr.src "http://i.imgur.com/Qr71crq.jpg"
+                                    , Attr.class "comment-author-img"
+                                    ]
+                                    []
+                                ]
+                            , a
+                                [ Attr.href ""
+                                , Attr.class "comment-author"
+                                ]
+                                [ text "Jacob Schmidt" ]
+                            , span
+                                [ Attr.class "date-posted"
+                                ]
+                                [ text "Dec 29th" ]
+                            ]
+                        ]
+                    , div
+                        [ Attr.class "card"
+                        ]
+                        [ div
+                            [ Attr.class "card-block"
+                            ]
+                            [ p
+                                [ Attr.class "card-text"
+                                ]
+                                [ text "With supporting text below as a natural lead-in to additional content." ]
+                            ]
+                        , div
+                            [ Attr.class "card-footer"
+                            ]
+                            [ a
+                                [ Attr.href ""
+                                , Attr.class "comment-author"
+                                ]
+                                [ img
+                                    [ Attr.src "http://i.imgur.com/Qr71crq.jpg"
+                                    , Attr.class "comment-author-img"
+                                    ]
+                                    []
+                                ]
+                            , a
+                                [ Attr.href ""
+                                , Attr.class "comment-author"
+                                ]
+                                [ text "Jacob Schmidt" ]
+                            , span
+                                [ Attr.class "date-posted"
+                                ]
+                                [ text "Dec 29th" ]
+                            , span
+                                [ Attr.class "mod-options"
+                                ]
+                                [ i
+                                    [ Attr.class "ion-edit"
+                                    ]
+                                    []
+                                , i
+                                    [ Attr.class "ion-trash-a"
+                                    ]
+                                    []
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
---
+++ src/Pages/Profile/Username_.elm
@@ -0,0 +1,267 @@
+module Pages.Profile.Username_ exposing (Model, Msg, page)
+
+import Effect exposing (Effect)
+import Html exposing (..)
+import Html.Attributes as Attr
+import Layout exposing (Layout)
+import Page exposing (Page)
+import Route exposing (Route)
+import Shared
+import View exposing (View)
+
+
+layout : Layout
+layout =
+    Layout.NavBar
+
+
+page : Shared.Model -> Route { username : String } -> Page Model Msg
+page shared route =
+    Page.new
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Effect Msg )
+init () =
+    ( {}
+    , Effect.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ExampleMsgReplaceMe
+
+
+update : Msg -> Model -> ( Model, Effect Msg )
+update msg model =
+    case msg of
+        ExampleMsgReplaceMe ->
+            ( model
+            , Effect.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    { title = "Pages.Profile.Username_"
+    , body = [ viewBody ]
+    }
+
+
+viewBody : Html Msg
+viewBody =
+    div
+        [ Attr.class "profile-page"
+        ]
+        [ div
+            [ Attr.class "user-info"
+            ]
+            [ div
+                [ Attr.class "container"
+                ]
+                [ div
+                    [ Attr.class "row"
+                    ]
+                    [ div
+                        [ Attr.class "col-xs-12 col-md-10 offset-md-1"
+                        ]
+                        [ img
+                            [ Attr.src "http://i.imgur.com/Qr71crq.jpg"
+                            , Attr.class "user-img"
+                            ]
+                            []
+                        , h4 []
+                            [ text "Eric Simons" ]
+                        , p []
+                            [ text "Cofounder @GoThinkster, lived in Aol's HQ for a few months, kinda looks like Peeta from the Hunger Games" ]
+                        , button
+                            [ Attr.class "btn btn-sm btn-outline-secondary action-btn"
+                            ]
+                            [ i
+                                [ Attr.class "ion-plus-round"
+                                ]
+                                []
+                            , text "Follow Eric Simons"
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        , div
+            [ Attr.class "container"
+            ]
+            [ div
+                [ Attr.class "row"
+                ]
+                [ div
+                    [ Attr.class "col-xs-12 col-md-10 offset-md-1"
+                    ]
+                    [ div
+                        [ Attr.class "articles-toggle"
+                        ]
+                        [ ul
+                            [ Attr.class "nav nav-pills outline-active"
+                            ]
+                            [ li
+                                [ Attr.class "nav-item"
+                                ]
+                                [ a
+                                    [ Attr.class "nav-link active"
+                                    , Attr.href ""
+                                    ]
+                                    [ text "My Articles" ]
+                                ]
+                            , li
+                                [ Attr.class "nav-item"
+                                ]
+                                [ a
+                                    [ Attr.class "nav-link"
+                                    , Attr.href ""
+                                    ]
+                                    [ text "Favorited Articles" ]
+                                ]
+                            ]
+                        ]
+                    , div
+                        [ Attr.class "article-preview"
+                        ]
+                        [ div
+                            [ Attr.class "article-meta"
+                            ]
+                            [ a
+                                [ Attr.href ""
+                                ]
+                                [ img
+                                    [ Attr.src "http://i.imgur.com/Qr71crq.jpg"
+                                    ]
+                                    []
+                                ]
+                            , div
+                                [ Attr.class "info"
+                                ]
+                                [ a
+                                    [ Attr.href ""
+                                    , Attr.class "author"
+                                    ]
+                                    [ text "Eric Simons" ]
+                                , span
+                                    [ Attr.class "date"
+                                    ]
+                                    [ text "January 20th" ]
+                                ]
+                            , button
+                                [ Attr.class "btn btn-outline-primary btn-sm pull-xs-right"
+                                ]
+                                [ i
+                                    [ Attr.class "ion-heart"
+                                    ]
+                                    []
+                                , text "29"
+                                ]
+                            ]
+                        , a
+                            [ Attr.href ""
+                            , Attr.class "preview-link"
+                            ]
+                            [ h1 []
+                                [ text "How to build webapps that scale" ]
+                            , p []
+                                [ text "This is the description for the post." ]
+                            , span []
+                                [ text "Read more..." ]
+                            ]
+                        ]
+                    , div
+                        [ Attr.class "article-preview"
+                        ]
+                        [ div
+                            [ Attr.class "article-meta"
+                            ]
+                            [ a
+                                [ Attr.href ""
+                                ]
+                                [ img
+                                    [ Attr.src "http://i.imgur.com/N4VcUeJ.jpg"
+                                    ]
+                                    []
+                                ]
+                            , div
+                                [ Attr.class "info"
+                                ]
+                                [ a
+                                    [ Attr.href ""
+                                    , Attr.class "author"
+                                    ]
+                                    [ text "Albert Pai" ]
+                                , span
+                                    [ Attr.class "date"
+                                    ]
+                                    [ text "January 20th" ]
+                                ]
+                            , button
+                                [ Attr.class "btn btn-outline-primary btn-sm pull-xs-right"
+                                ]
+                                [ i
+                                    [ Attr.class "ion-heart"
+                                    ]
+                                    []
+                                , text "32"
+                                ]
+                            ]
+                        , a
+                            [ Attr.href ""
+                            , Attr.class "preview-link"
+                            ]
+                            [ h1 []
+                                [ text "The song you won't ever stop singing. No matter how hard you try." ]
+                            , p []
+                                [ text "This is the description for the post." ]
+                            , span []
+                                [ text "Read more..." ]
+                            , ul
+                                [ Attr.class "tag-list"
+                                ]
+                                [ li
+                                    [ Attr.class "tag-default tag-pill tag-outline"
+                                    ]
+                                    [ text "Music" ]
+                                , li
+                                    [ Attr.class "tag-default tag-pill tag-outline"
+                                    ]
+                                    [ text "Song" ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ]
