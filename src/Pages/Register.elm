10b33b8a7479be5de003dd31c94af844fc7d8d44
Pravin Rahangdale <pravinrahangdale41@gmail.com>
2022-10-23 00:57:46 +0530

    Add register api

--- src/Pages/Register.elm
+++ src/Pages/Register.elm
@@ -1,14 +1,20 @@
 module Pages.Register exposing (Model, Msg, page)
 
+import Api.User
 import Effect exposing (Effect)
 import Html exposing (..)
 import Html.Attributes as Attr
+import Html.Events
+import Http
+import Json.Decode
+import Json.Encode
 import Layout exposing (Layout)
 import Page exposing (Page)
 import Route exposing (Route)
 import Shared
+import Shared.Msg
 import View exposing (View)
-import Auth.Action
+
 
 layout : Layout
 layout =
@@ -30,12 +36,22 @@ page shared route =
 
 
 type alias Model =
-    {}
+    { email : String
+    , password : String
+    , username : String
+    , errors : List FormError
+    , isSubmittingForm : Bool
+    }
 
 
 init : () -> ( Model, Effect Msg )
 init () =
-    ( {}
+    ( { email = ""
+      , password = ""
+      , username = ""
+      , errors = []
+      , isSubmittingForm = False
+      }
     , Effect.none
     )
 
@@ -44,15 +60,105 @@ init () =
 -- UPDATE
 
 
+type alias RegisterUser =
+    { username : String
+    , email : String
+    , password : String
+    }
+
+
+type alias RegisterUserResponse =
+    { username : String
+    , token : String
+    , email : String
+    , bio : String
+    , image : String
+    }
+
+
+type Field
+    = Email
+    | Password
+    | UserName
+
+
+type alias FormError =
+    { field : Maybe Field
+    , message : String
+    }
+
+
 type Msg
-    = ExampleMsgReplaceMe
+    = RegisterUserApiResponded (Result (List FormError) String)
+    | UserApiResponded (Result Http.Error Api.User.User)
+    | UserUpdatedInput Field String
+    | UserSubmittedForm
 
 
 update : Msg -> Model -> ( Model, Effect Msg )
 update msg model =
     case msg of
-        ExampleMsgReplaceMe ->
+        UserSubmittedForm ->
+            ( { model
+                | isSubmittingForm = True
+                , errors = []
+              }
+            , Effect.fromCmd
+                (callRegisterApi
+                    { username = model.username
+                    , email = model.email
+                    , password = model.password
+                    }
+                )
+            )
+
+        RegisterUserApiResponded (Err formErrors) ->
+            ( { model | errors = formErrors, isSubmittingForm = False }
+            , Effect.none
+            )
+
+        RegisterUserApiResponded (Ok token) ->
             ( model
+            , Effect.batch
+                [ Effect.save
+                    { key = "token"
+                    , value = Json.Encode.string token
+                    }
+                , Effect.fromCmd
+                    (Api.User.getCurrentUser
+                        { token = token
+                        , onResponse = UserApiResponded
+                        }
+                    )
+                ]
+            )
+
+        UserApiResponded result ->
+            ( model
+            , Effect.fromSharedMsg (Shared.Msg.SignInPageSignedInUser result)
+            )
+
+        UserUpdatedInput Email value ->
+            ( { model
+                | email = value
+                , errors = clearErrorsFor Email model.errors
+              }
+            , Effect.none
+            )
+
+        UserUpdatedInput UserName value ->
+            ( { model
+                | username = value
+                , errors = clearErrorsFor UserName model.errors
+              }
+            , Effect.none
+            )
+
+        UserUpdatedInput Password value ->
+            ( { model
+                | password = value
+                , errors = clearErrorsFor Password model.errors
+              }
             , Effect.none
             )
 
@@ -109,7 +215,7 @@ viewBody =
                         [ li []
                             [ text "That email is already taken" ]
                         ]
-                    , form []
+                    , form [ Html.Events.onSubmit UserSubmittedForm ]
                         [ fieldset
                             [ Attr.class "form-group"
                             ]
@@ -117,6 +223,8 @@ viewBody =
                                 [ Attr.class "form-control form-control-lg"
                                 , Attr.type_ "text"
                                 , Attr.placeholder "Your Name"
+                                , Attr.required True
+                                , Html.Events.onInput (UserUpdatedInput UserName)
                                 ]
                                 []
                             ]
@@ -127,6 +235,8 @@ viewBody =
                                 [ Attr.class "form-control form-control-lg"
                                 , Attr.type_ "text"
                                 , Attr.placeholder "Email"
+                                , Attr.required True
+                                , Html.Events.onInput (UserUpdatedInput Email)
                                 ]
                                 []
                             ]
@@ -137,6 +247,8 @@ viewBody =
                                 [ Attr.class "form-control form-control-lg"
                                 , Attr.type_ "password"
                                 , Attr.placeholder "Password"
+                                , Attr.required True
+                                , Html.Events.onInput (UserUpdatedInput Password)
                                 ]
                                 []
                             ]
@@ -149,3 +261,108 @@ viewBody =
                 ]
             ]
         ]
+
+
+
+-- Api Call
+
+
+callRegisterApi : RegisterUser -> Cmd Msg
+callRegisterApi registerUser =
+    let
+        json : Json.Encode.Value
+        json =
+            Json.Encode.object
+                [ ( "user"
+                  , Json.Encode.object
+                        [ ( "username", Json.Encode.string registerUser.username )
+                        , ( "email", Json.Encode.string registerUser.email )
+                        , ( "password", Json.Encode.string registerUser.password )
+                        ]
+                  )
+                ]
+
+        userDecoder =
+            Json.Decode.field "user" tokenDecoder
+
+        tokenDecoder : Json.Decode.Decoder String
+        tokenDecoder =
+            Json.Decode.field "token" Json.Decode.string
+    in
+    Http.post
+        { url = "https://api.realworld.io/api/users"
+        , body = Http.jsonBody json
+        , expect = expectApiResponse RegisterUserApiResponded userDecoder
+        }
+
+
+expectApiResponse :
+    (Result (List FormError) value -> msg)
+    -> Json.Decode.Decoder value
+    -> Http.Expect msg
+expectApiResponse toMsg decoder =
+    Http.expectStringResponse toMsg (toFormApiResult decoder)
+
+
+toFormApiResult : Json.Decode.Decoder value -> Http.Response String -> Result (List FormError) value
+toFormApiResult decoder response =
+    case response of
+        Http.BadUrl_ _ ->
+            Err [ { field = Nothing, message = "Unexpected URL format" } ]
+
+        Http.Timeout_ ->
+            Err [ { field = Nothing, message = "Server did not respond" } ]
+
+        Http.NetworkError_ ->
+            Err [ { field = Nothing, message = "Could not connect to server" } ]
+
+        Http.BadStatus_ { statusCode } rawJson ->
+            case Json.Decode.decodeString formErrorsDecoder rawJson of
+                Ok errors ->
+                    Err errors
+
+                Err _ ->
+                    Err [ { field = Nothing, message = "Received status code " ++ String.fromInt statusCode } ]
+
+        Http.GoodStatus_ _ rawJson ->
+            case Json.Decode.decodeString decoder rawJson of
+                Ok value ->
+                    Ok value
+
+                Err _ ->
+                    Err [ { field = Nothing, message = "Received unexpected API response" } ]
+
+
+formErrorsDecoder : Json.Decode.Decoder (List FormError)
+formErrorsDecoder =
+    let
+        formErrorDecoder : Json.Decode.Decoder FormError
+        formErrorDecoder =
+            Json.Decode.map2 FormError
+                (Json.Decode.field "field" Json.Decode.string
+                    |> Json.Decode.map fromStringToMaybeField
+                )
+                (Json.Decode.field "message" Json.Decode.string)
+
+        fromStringToMaybeField : String -> Maybe Field
+        fromStringToMaybeField field =
+            case field of
+                "email" ->
+                    Just Email
+
+                "password" ->
+                    Just Password
+
+                "username" ->
+                    Just UserName
+
+                _ ->
+                    Nothing
+    in
+    Json.Decode.field "errors" (Json.Decode.list formErrorDecoder)
+
+
+clearErrorsFor : Field -> List FormError -> List FormError
+clearErrorsFor field errors =
+    errors
+        |> List.filter (\error -> error.field /= Just field)
