8be83cdbf9dd078206c0ab7f417fbfb99e87df29
Pravin Rahangdale <pravinrahangdale41@gmail.com>
2022-11-05 01:01:03 +0530

    Add follow buton to profile

---
+++ src/Api/Profile.elm
@@ -0,0 +1,97 @@
+module Api.Profile exposing (followUser, getProfile, unFollowUser, Profile)
+
+import Effect exposing (Effect)
+import Http
+import Json.Decode exposing (bool, maybe, string)
+import Json.Decode.Pipeline exposing (required)
+
+
+type alias Profile =
+    { username : String
+    , bio : Maybe String
+    , image : String
+    , following : Bool
+    }
+
+
+followUser :
+    { onResponse : Result Http.Error Profile -> msg
+    , token : Maybe String
+    , username : String
+    }
+    -> Effect msg
+followUser options =
+    let
+        headers =
+            case options.token of
+                Just token ->
+                    [ Http.header "Authorization" ("Bearer " ++ token) ]
+
+                Nothing ->
+                    []
+    in
+    Effect.fromCmd
+        (Http.request
+            { method = "POST"
+            , url = "https://api.realworld.io/api/profiles/" ++ options.username ++ "/follow"
+            , expect = Http.expectJson options.onResponse profileDecoder
+            , body = Http.emptyBody
+            , timeout = Nothing
+            , tracker = Nothing
+            , headers = headers
+            }
+        )
+
+
+unFollowUser :
+    { onResponse : Result Http.Error Profile -> msg
+    , token : Maybe String
+    , username : String
+    }
+    -> Effect msg
+unFollowUser options =
+    let
+        headers =
+            case options.token of
+                Just token ->
+                    [ Http.header "Authorization" ("Bearer " ++ token) ]
+
+                Nothing ->
+                    []
+    in
+    Effect.fromCmd
+        (Http.request
+            { method = "DELETE"
+            , url = "https://api.realworld.io/api/profiles/" ++ options.username ++ "/follow"
+            , expect = Http.expectJson options.onResponse profileDecoder
+            , body = Http.emptyBody
+            , timeout = Nothing
+            , tracker = Nothing
+            , headers = headers
+            }
+        )
+
+
+profileDecoder : Json.Decode.Decoder Profile
+profileDecoder =
+    Json.Decode.field "profile"
+        (Json.Decode.succeed Profile
+            |> required "username" string
+            |> required "bio" (maybe string)
+            |> required "image" string
+            |> required "following" bool
+        )
+
+
+getProfile :
+    { onResponse : Result Http.Error Profile -> msg
+    , username : String
+    }
+    -> Effect msg
+getProfile { onResponse, username } =
+    Effect.fromCmd
+        (Http.get
+            { url = "https://api.realworld.io/api/profiles/" ++ username
+            , expect = Http.expectJson onResponse profileDecoder
+            }
+        )
